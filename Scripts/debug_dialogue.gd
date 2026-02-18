8a74715c5222ea6f19024e1463e7dc0f7e3a6c42
vikbro <vikso.todorov@gmail.com>
2026-02-18 14:03:06 +0200

    Started dialogue

---
+++ Scripts/debug_dialogue.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_paper_button_pressed() -> void:
+	Dialogic.start("paper_dialogue")
+	pass # Replace with function body.
+
+func _on_moon_button_pressed() -> void:
+	#Dialogic.start("paper_dialogue")
+	
+	pass # Replace with function body.
