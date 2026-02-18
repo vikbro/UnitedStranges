7813f1077b84b4f395188cc4dc5347c950d16b78
vikbro <vikso.todorov@gmail.com>
2026-02-18 18:03:40 +0200

    Added phase change animation

--- Scripts/debug_dialogue_phase.gd
+++ Scripts/debug_dialogue_phase.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const DIALOGUE_PHASE_LEVEL = preload("uid://sccvf5bptkd3")
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +15,12 @@ func _process(delta: float) -> void:
 
 
 func _on_paper_transport_pressed() -> void:
+	animation_player.play("show_text_panel")
+	await animation_player.animation_finished
+	animation_player.play("diplomacy_slide")
+	await animation_player.animation_finished
+	animation_player.play("fade_out_text_panel")
+	await animation_player.animation_finished
 	
 	SceneTransition.change_scene_to_node(DIALOGUE_PHASE_LEVEL.instantiate(),DiplomacyManager.kingdoms[KingdomStats.Type.PAPER].background_img)
 	
